22688c8150017eb2b9693dfb74f09553d16485e0
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2024-01-22 08:21:11 -0800

    completed chapter 16

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-let js = "amazing";
+/*let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 console.log('Jonas');
@@ -23,3 +23,83 @@ let job1 = 'Programmer';
 let job2 = 'Teacher';
 
 console.log(myFirstJob)
+*/
+
+/*console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+/*let age = 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear = 1990;
+
+//const job;
+var job = 'programmer';
+job = 'teacher'
+
+lastName = 'Romero';
+console.log(lastName);
+*/
+//math operators
+/*
+const now = 2037;
+const ageRomero = now - 1988;
+const ageSarah = now - 2018;
+console.log(ageRomero, ageSarah);
+
+console.log(ageRomero * 2, ageRomero / 10, 2 ** 3)
+//2**3 means 2 to the power of 3 = 2*2*2
+
+const firstName = 'Oscar';
+const lastName = 'Romero';
+
+console.log(firstName + ' ' + lastName);
+*/
+
+/*
+//assignment operators 
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; //x = x + 1 = 101
+x--;
+x--;
+console.log(x);
+
+// comparitive operators
+console.log(ageRomero > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageRomero = now - 1988;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageRomero + ageSarah) / 2
+console.log(ageRomero, ageSarah, averageAge);
