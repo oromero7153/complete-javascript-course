fd13a8545d4a932c3385d2b08750c02207a159ef
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2024-02-06 09:34:58 -0800

    completed video 45 again

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -319,8 +319,8 @@ const mark = {
     fullName: 'Mark Miller',
     mass: 78,
     height: 1.69,
-    calcBmi: function () {
-        this.bmi = (this.mass / (this.height * this.height)).toFixed(2);
+    calcBMI: function () {
+        this.bmi = (this.mass / (this.height ** 2)).toFixed(2);
         return this.bmi
     }
 };
@@ -329,11 +329,21 @@ const john = {
     fullName: 'John Smith',
     mass: 92,
     height: 1.95,
-    calcBmi: function () {
-        this.bmi = (this.mass / (this.height * this.height)).toFixed(2);
+    calcBMI: function () {
+        this.bmi = (this.mass / (this.height ** 2)).toFixed(2);
         return this.bmi
     }
 };
 
-console.log(`${john.fullName}'s BMI (${john.calcBmi()}) is higher than ${mark.fullName}'s BMI (${mark.calcBmi()})!`)
+mark.calcBMI();
+john.calcBMI();
 
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})!`)
+} else if (john.bmi > mark.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})!`)
+} else {
+    console.log(`They have the same BMI`)
+}
