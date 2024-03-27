038f0ea26048bcb8589c3e66631a444e0a0ae393
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2024-03-27 07:33:40 -0700

    completed video 118

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -52,6 +52,73 @@ const restaurant = {
   },
 };
 
+////////////////maps:fundamentals////////
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+rest.set(2, 'Lisbonm Portugal');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close,23')
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+//rest.clear();
+console.log(rest);
+console.log(rest.size);
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+/*
+/////////////////Sets///////////////////
+const ordersSet = new Set([
+  'pasta',
+  'pizza',
+  'pizza',
+  'risotto',
+  'pasta',
+  'pizza',
+]);
+console.log(ordersSet);
+
+console.log(new Set());
+console.log(ordersSet.size); //tells you how many objects are in a set, no repeats
+console.log(ordersSet.has('pizza')); //checks if there is a specific object in the set
+console.log(ordersSet.has('bread'));
+ordersSet.add('garlic bread'); // adds objects to a set
+ordersSet.delete('risotto'); // removes objects from a set
+//ordersSet.clear(); deletes everything
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order); // you can loop a set
+
+//example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)]; // the spread operator turns the set into an array.
+console.log(staffUnique);
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+);
+
+/*
 //////////////////lopping opjects; object keys, values and entries
 //Property names
 const properties = Object.keys(openingHours);
@@ -365,7 +432,7 @@ const [[, rating], [, ratingscount]] = ratings;
 
 const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
 */
-
+/*
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -460,3 +527,4 @@ for (const [team, odd] of Object.entries(game.odds)) {
 // Odd of victory Bayern Munich: 1.33
 //       Odd of draw: 3.25
 //       Odd of victory Borrussia Dortmund: 6.5
+*/
