c2c5cc0c6a05c58bd135b213e701e4de8bb3a2db
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2024-11-14 09:47:27 -0800

    completed video 55

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,6 +1,5 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
-
 /*
 // PROBLEM 1:
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
