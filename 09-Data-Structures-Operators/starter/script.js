39564c19a6c6abd7c3416dbc1c81215f045409e5
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2024-12-03 11:31:52 -0800

    completed video 124

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -53,6 +53,266 @@ const restaurant = {
   },
 };
 /*
+/////working with strings 3
+//Split and Join
+console.log('a+very+nice+string'.split('+'));
+console.log('Oscar Romero'.split(' '));
+
+const [firstName, lastName] = 'Oscar Romero'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeNanme = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+  for (const n of names) {
+    //namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+capitalizeNanme('jessica ann smith davis'); // should log Jessica Ann Smith Davis
+capitalizeNanme('oscar romero'); // should log Oscar Romero
+
+//padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(25, '+')); // should log +++++++++++Go to gate 23!
+console.log('Jonas'.padEnd(25, '+')); // should log Jonas++++++++++++++++++++
+console.log(message.padStart(25, '+').padEnd(30, '+')); // should log +++++++++++Go to gate 23!+++++
+
+const maskCreditCard = function (number) {
+  const str = number + ''; // converts it to a sting as well.
+  const last = str.slice(-4);
+  const ccNumber = last.padStart(str.length, '*');
+  console.log(ccNumber);
+};
+
+maskCreditCard(51235425); //should log ****5425
+maskCreditCard(5123665423239865); //should log ************9865
+maskCreditCard('372255566985547745'); //should log **************7745
+
+//repeat
+const message2 = 'Bad Weather.... All Departures are delayed... ';
+console.log(message2.repeat(5)); //should log 5X of "Bad Weather.... All Departures are delayed..."
+
+const planesInLine = function (n) {
+  console.log(`there are ${n} planes in line ${'🛬'.repeat(n)}!`);
+};
+planesInLine(5); //should log there are 5 planes in line 🛬🛬🛬🛬🛬!
+planesInLine(3); //should log there are 5 planes in line 🛬🛬🛬!
+planesInLine(12); //should log there are 12 planes in line 🛬🛬🛬🛬🛬🛬🛬🛬🛬🛬🛬🛬!
+/*
+/////working with strings 2
+const airline = 'Air Canada';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// fix capitalization
+const passenger = 'jOnAs'; // should be Jonas
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+//comparing emails
+const email = 'hello@jonas.io';
+const loginEmail = '   Hello@Jonas.io \n';
+
+// const lowerEmail = loginEmail.toLocaleLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+
+const normalizedEmail = loginEmail.toLocaleLowerCase().trim();
+console.log(normalizedEmail);
+
+console.log(email === normalizedEmail ? 'true' : 'false');
+
+//replacing
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+//replaces all occurences
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+
+//console.log(announcement.replaceAll('door', 'gate'));
+console.log(announcement.replace(/door/g, 'gate'));
+
+//booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Air'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('part of the new Airbus Family');
+}
+
+//example
+const checkBaggage = function (items) {
+  const baggage = items.toLocaleLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board!');
+  } else {
+    console.log('You are allowed on board!');
+  }
+};
+
+checkBaggage('I have a laptop, some Food, and a pocket Knife.');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection.');
+
+/*
+/////working with strings 1
+const airline = 'Air Canada';
+const plane = 'A320';
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // b and e are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') {
+    // '||' = OR
+    console.log('You got the middle seat!');
+  } else {
+    console.log('You got Lucky!');
+  }
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('jonas'));
+console.log(typeof new String('jonas'));
+
+console.log(typeof new String('jonas').slice(1));
+
+/*
+///////// Maps: Iteration
+const question = new Map([
+  ['question', 'what is the best programming language in the world?'],
+  [1, 'c'],
+  [2, 'java'],
+  [3, 'javascript'],
+  ['correct', 3],
+  [true, 'correct!'],
+  [false, 'try again!'],
+]);
+console.log(question);
+
+//convert objects to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`answer ${key}: ${value}`);
+}
+const answer = 3;
+
+console.log(question.get(question.get('correct') === answer));
+
+//convert map to array
+console.log([...question]);
+//console.log([...question.entries()]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+/*
+///////// Maps: Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'firenze, italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('catergories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('closed', 23)
+  .set(true, 'we are open')
+  .set(false, 'we are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
+//rest.clear();
+console.log(rest);
+
+const arr = [1, 2];
+rest.set(arr, 'test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+/*
+///////// Sets
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+console.log(ordersSet);
+
+console.log(new Set(''));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('garlic bread');
+ordersSet.add('garlic bread');
+ordersSet.delete('Risotto');
+// ordersSet.clear();   deletes everything from a set
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+//example
+const staff = ['Waiter', 'Chef', 'Waiter', 'manager', 'Chef', 'Waiter'];
+const stuffUnique = [...new Set(staff)];
+console.log(stuffUnique);
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'manager', 'Chef', 'Waiter']).size
+);
+
+console.log(new Set('oscarromero').size);
+
+/*
 ///////// Looping objects: Object Keys, value and entries
 
 //Property names
@@ -485,7 +745,7 @@ BONUS: Create an object called 'scorers' which contains the names of the players
         Lewandowski: 2
       };
       */
-
+/*
 // 1.
 const goalScored = Object.entries(game.scored);
 // console.log(goalScored);
@@ -501,3 +761,93 @@ let average = 0;
 for (const odd of odds) average += odd;
 average /= odds.length;
 console.log(average);
+
+//3.
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr}: ${odd}`);
+}
+*/
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+/*
+//1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+//2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//3.
+console.log(
+  `An event happened, on average every ${90 / gameEvents.size} minutes`
+);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+
+console.log(
+  `An event happened, on average every ${time / gameEvents.size} minutes`
+);
+
+//4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND'; //ternary operator
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
+*/
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
