273e71e5d744a376702aaf111f98b4e185dfb726
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2024-12-05 09:38:45 -0800

    completed video 140

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -222,7 +222,7 @@ BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 
 GOOD LUCK 😀
 */
-
+/*
 const poll = {
   question: 'What is your favourite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
@@ -260,9 +260,102 @@ document
 poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 
-// const displayResults = function () {
-//   console.log(
-//     `Poll results are ${poll.answers[0]}, ${poll.answers[1]}, ${poll.answers[2]}, ${poll.answers[3]}.`
-//   );
-// };
-// displayResults();
+*/
+/*
+/////////// Immediately invoked function expressions (IIFE)
+
+const runOnce = function () {
+  console.log('this techincally can run again');
+};
+runOnce();
+
+//IIFE
+(function () {
+  console.log('this will never run again');
+})();
+
+(() => console.log('this will ALSO never run again'))();
+*/
+
+/*
+/////////// Closures
+const secureBooking = function () {
+  let passerngerCount = 0;
+
+  return function () {
+    passerngerCount++;
+    console.log(`${passerngerCount} passengers`);
+  };
+};
+const booker = secureBooking();
+booker();
+booker();
+booker();
+// a function has access to a variable environment (VE) of the execution context in which it was created, even after that execution context is gone;
+//the booker function has access to the passenger count variable. This is called closure. Closure has priority over the scope chain.
+//console.dir(booker);
+
+// closure can change/ be reassigned
+//Example 1
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// re-assigning f function
+h();
+f();
+console.dir(f);
+
+//example 2
+const boardPassenger = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers.`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers.`);
+  }, 1000);
+
+  console.log(`Will starting boarding in ${wait} seconds.`);
+};
+
+//const perGroup = 1000; the closure will have priority over scope chain. 
+boardPassenger(180, 3);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  console.log('this executed');
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
